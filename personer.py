09539d048c9e701ecaf1db015173e4447baa577c
Christian326 <148755386+Christian326@users.noreply.github.com>
2023-11-03 09:45:12 +0100

    mine endringer

---
+++ personer.py
@@ -0,0 +1,8 @@
+class Person:
+    "En klasse for å beskrive en person"
+    def __init__(self,navn:str,årstall:int,høyde:float,vekt:float) -> None
+
+        self.navn = navn
+        self.årstall = årstall
+        self.høyde = høyde
+        self.vekt = vekt
--- planeter.py
+++ planeter.py
@@ -1,15 +1,42 @@
 
 class Planet:
     " En klasse for å beskrive en planet"
+<<<<<<< Updated upstream
     def __init__(self,navn:str,solavstand:float,radius:float,antallRinger=0) -> None:
+=======
+    def __init__(self,navn:str,solavstand:float,radius:float, antallRinger=0) -> None:
+>>>>>>> Stashed changes
 
         self.navn=navn
         self.solavstand=solavstand
         self.radius = radius
+<<<<<<< Updated upstream
         self.antallRinger=antallRinger
 
 jorden = Planet( "Jorden", 152, 6371)
 
+=======
+        self.antallRinger = antallRinger
+
+jorden = Planet( "Jorden", 152, 6371)
+
+mars=Planet("Mars",222.9,3389.5)
+
+jupiter = ("Jupiter", 778, 69911)
+
+print(type(str))
+
+#Notater
+#Klassen planet
+#Navn
+#Radius og størrelse
+#Avstand fra sola
+#Antall ringer
+#Farge?
+#Beboelig / befolkning
+#Temperatur
+#++++
+>>>>>>> Stashed changes
 
 # Oppgave 1: 
 # Lag et objekt for Mars, Jupiter og Jorda, der du lagrer informasjon om navn, solavstand og radius. Lagre disse objektene i egne variabler
